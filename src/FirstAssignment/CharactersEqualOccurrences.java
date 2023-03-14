b363014bb65228ed6a37bc60495943e735fff848
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-14 14:06:48 +0530

    2nd commit - as per PR comment, formated the code

--- src/FirstAssignment/CharactersEqualOccurrences.java
+++ src/FirstAssignment/CharactersEqualOccurrences.java
@@ -1,35 +1,29 @@
 package FirstAssignment;
 
-
 public class CharactersEqualOccurrences {
 
 	public static void main(String[] args) {
 		boolean b1;
 		b1 = areOccurrencesEqual("abacbc");
-		System.out.println("String abacbc is : "+b1);
+		System.out.println("String abacbc is : " + b1);
 		b1 = areOccurrencesEqual("aaabb");
-		System.out.println("String aaabb is : "+b1);
+		System.out.println("String aaabb is : " + b1);
 	}
 
-	public static boolean areOccurrencesEqual(String s) { 
-
+	public static boolean areOccurrencesEqual(String s) {
 		int[] counts = new int[26];
-		int max=0;
+		int max = 0;
 
-		for(char c : s.toCharArray())
-		{
-			counts[c-'a']++;
-			max=Math.max(max, counts[c-'a']);
+		for (char c : s.toCharArray()) {
+			counts[c - 'a']++;
+			max = Math.max(max, counts[c - 'a']);
 		}
 
-		for(int count:counts)
-		{
-			if(count!=0 && count !=max)
-			{
+		for (int count : counts) {
+			if (count != 0 && count != max) {
 				return false;
 			}
 		}
 		return true;
-	} 
-
+	}
 }
