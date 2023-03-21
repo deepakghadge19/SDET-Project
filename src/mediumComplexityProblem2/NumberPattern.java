1cbaa2a226732ea744481269ea03b4059f7fc5f8
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-21 20:48:57 +0530

    added code for medium complexity problem2- Number pattern

---
+++ src/mediumComplexityProblem2/NumberPattern.java
@@ -0,0 +1,26 @@
+package mediumComplexityProblem2;
+
+public class NumberPattern {
+    private int i, j, row = 4;
+
+    public void pattern() {
+        for (i = 1; i <= row; i++) {
+            for (j = 1; j <= row - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberPattern p1 = new NumberPattern();
+        p1.pattern();
+    }
+
+}
