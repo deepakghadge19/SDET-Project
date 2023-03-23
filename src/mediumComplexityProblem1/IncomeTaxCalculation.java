b0e864130a3e60c581f21e06fff7863d18c8cbbe
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-23 18:43:29 +0530

    Updated code according to PR comments

--- src/mediumComplexityProblem1/IncomeTaxCalculation.java
+++ src/mediumComplexityProblem1/IncomeTaxCalculation.java
@@ -1,30 +1,36 @@
 package mediumComplexityProblem1;
+/*Q.1 Write a java program to find tax for employee. The program should
+calculate the income tax to be paid by the employee as per the criteria given below:
+Slab rate IT rate
+Upto Rs. 7,00,000 Nil
+Above Rs. 7,00,000 (0-5,00,000=nil,5,00,000 – 7,00,000=10%)
+Rs. 7,00,000-10,00,000 15% on additional amount
+Rs. 10,00,000-15,00,000 20% on additional amount Above 15,00,000 30% on additional amount
+Result: - income tax is ……………………………. */
 
 import java.util.Scanner;
 
 public class IncomeTaxCalculation {
-    private double tax;
-    private double it;
+    private double totalTax;
+    private double taxableIncome;
 
-    public void findIncomeTax() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter income :");
-        it = sc.nextDouble();
-        if (it <= 700000) {
-            tax = 0;
-        } else if (it > 500000 && it <= 1000000) {
-            tax = (0.15 * (it - 700000)) + (0.1 * 200000);
-        } else if (it <= 1500000) {
-            tax = (0.2 * (it - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
+    public void findIncomeTax(double taxableIncome) {
+        this.taxableIncome = taxableIncome;
+        if (this.taxableIncome <= 700000) {
+            totalTax = 0;
+        } else if (this.taxableIncome > 500000 && this.taxableIncome <= 1000000) {
+            totalTax = (0.15 * (this.taxableIncome - 700000)) + (0.1 * 200000);
+        } else if (this.taxableIncome <= 1500000) {
+            totalTax = (0.2 * (this.taxableIncome - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
         } else {
-            tax = (0.3 * (it - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
+            totalTax = (0.3 * (this.taxableIncome - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
         }
-        System.out.println("Income tax amount is :" + tax);
+        System.out.println("Income tax amount is :" + totalTax);
     }
 
     public static void main(String[] args) {
-        IncomeTaxCalculation a = new IncomeTaxCalculation();
-        a.findIncomeTax();
+        IncomeTaxCalculation incomeTaxCalculation = new IncomeTaxCalculation();
+        incomeTaxCalculation.findIncomeTax(1600000);
     }
 }
 /*OUTPUT :
