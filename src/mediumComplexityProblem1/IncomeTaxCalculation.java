e6151a5ffb52f18c622d98e38dbf1d2612c7b76d
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-29 12:15:43 +0530

    Updated code for 'Switch Case' and also added Readme file for input and output

--- src/mediumComplexityProblem1/IncomeTaxCalculation.java
+++ src/mediumComplexityProblem1/IncomeTaxCalculation.java
@@ -1,27 +1,28 @@
 package mediumComplexityProblem1;
-/*Q.1 Write a java program to find tax for employee. The program should
-calculate the income tax to be paid by the employee as per the criteria given below:
-Slab rate IT rate
-Upto Rs. 7,00,000 Nil
-Above Rs. 7,00,000 (0-5,00,000=nil,5,00,000 – 7,00,000=10%)
-Rs. 7,00,000-10,00,000 15% on additional amount
-Rs. 10,00,000-15,00,000 20% on additional amount Above 15,00,000 30% on additional amount
-Result: - income tax is ……………………………. */
 
 public class IncomeTaxCalculation {
     private double totalTax;
-    private double taxableIncome;
+    private int checkedCase;
 
-    public void findIncomeTax(double taxableIncome) {
-        this.taxableIncome = taxableIncome;
-        if (this.taxableIncome <= 700000) {
-            totalTax = 0;
-        } else if (this.taxableIncome > 500000 && this.taxableIncome <= 1000000) {
-            totalTax = (0.15 * (this.taxableIncome - 700000)) + (0.1 * 200000);
-        } else if (this.taxableIncome <= 1500000) {
-            totalTax = (0.2 * (this.taxableIncome - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
-        } else {
-            totalTax = (0.3 * (this.taxableIncome - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
+    public void findIncomeTax(int taxableIncome) {
+        checkedCase = (taxableIncome <= 700000) ? 1 : (taxableIncome > 500000 && taxableIncome <= 1000000) ?
+                2 : (taxableIncome <= 1500000) ? 3 : 4;
+
+        switch (checkedCase) {
+            case 1:
+                totalTax = 0;
+                break;
+            case 2:
+                totalTax = (0.15 * (taxableIncome - 700000)) + (0.1 * 200000);
+                break;
+            case 3:
+                totalTax = (0.2 * (taxableIncome - 1000000)) + (0.15 * 300000) + (0.1 * 200000);
+                break;
+            case 4:
+                totalTax = (0.3 * (taxableIncome - 1500000)) + (0.2 * 500000) + (0.15 * 300000) + (0.1 * 200000);
+                break;
+            default:
+                System.out.println("Enter valid taxable income");
         }
         System.out.println("Income tax amount is :" + totalTax);
     }
@@ -38,16 +39,3 @@ public class IncomeTaxCalculation {
         incomeTaxCalculation.findIncomeTax(1600000);
     }
 }
-/*OUTPUT :
-Total Taxable Income : 700000
-Income tax amount is :0.0
-
-Total Taxable Income : 800000
-Income tax amount is :35000.0
-
-Total Taxable Income : 1400000
-Income tax amount is :145000.0
-
-Total Taxable Income : 1600000
-Income tax amount is :195000.0
- */
