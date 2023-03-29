bc4679d8d0d12af1580b3b9a584dcecd8f621e7b
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-29 15:49:08 +0530

    removed for loop and flag from code as per PR comment

--- src/mediumComplexityProblem3/SwapNumbers.java
+++ src/mediumComplexityProblem3/SwapNumbers.java
@@ -1,38 +1,22 @@
 package mediumComplexityProblem3;
-/*Q3. Given an array of ints, swap the first and last elements in the array. Return the modified array.
-If the last array value is prime no.
-swapEnds([1, 2, 3, 7]) → [7, 2, 3, 1]
-swapEnds([1, 2, 3]) → [3, 2, 1]
-swapEnds([1, 6, 7, 9, 8]) → [1, 6, 7, 9, 8] */
 
 import java.util.Arrays;
 
 public class SwapNumbers {
-    private int temp;
-    private int lastNum;
-    private boolean flag = false;
 
     public void swapEnds(int[] arr) {
         //To find last array value
-        lastNum = arr[arr.length - 1];
-        //To check whether the last array value is a prime number
-        for (int i = 2; i <= (lastNum / 2); ++i) {
-            if (lastNum % i == 0) {
-                flag = true;
-                System.out.println("Last Array value:" + lastNum + " is not a prime number");
-                break;
-            }
-        }
+        int lastNum = arr[arr.length - 1];
+
         //Swap the first and last elements in the array,If the last array value is prime number
-        if (flag == false) {
+        if (lastNum % 2 == 0) {
+            System.out.println("Last Array value:" + lastNum + " is not a prime number so elements are not swapped");
+        } else {
             System.out.println("Last Array value:" + lastNum + " is a prime number.");
-            temp = arr[0];
+            int temp = arr[0];
             arr[0] = lastNum;
             arr[arr.length - 1] = temp;
-            //printing modified array
             System.out.println("Modified Array after swap the first and last elements :" + Arrays.toString(arr));
-        } else {
-            System.out.println("Last Array value:" + lastNum + " is not a prime number so elements are not swapped");
         }
     }
 
