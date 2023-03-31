499424240bcefc19e2ea88e60da5baa041ccae58
deepakghadge <deepak.ghadge@excellarate.com>
2023-03-31 12:05:07 +0530

    Updated code as per the PR comments

--- src/secondAssignment_14March2023/FindDestinationCity.java
+++ src/secondAssignment_14March2023/FindDestinationCity.java
@@ -5,18 +5,29 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class FindDestinationCity {
+    public void destinationCity(String[][] input) {
+        ArrayList<String> city = new ArrayList<>();
+        for (int row = 0; row < input.length; row++) {
+            for (int column = 0; column < 2; column++) {
+                city.add(input[row][column]);
+            }
+        }
+        //To find unique and ordered city
+        Set<String> uniqueCity = new LinkedHashSet<>(city);
+        ArrayList<String> destinationCity = new ArrayList<>(uniqueCity);
+        System.out.println("Destination city is :" + destinationCity.get(destinationCity.size() - 1));
+    }
 
-	public static void main(String[] args) {
-		String input[][] = { { "London", "Newyork" }, { "Newyork", "Lima" }, { "Lima", "Sao Paulo" } };
-		// String input[][] = { { "B", "C" }, { "D", "B" }, { "C", "A" } };
-		ArrayList<String> AL = new ArrayList<>();
-		for (int i = 0; i < input.length; i++) {
-			for (int j = 0; j < 2; j++) {
-				AL.add(input[i][j]);
-			}
-		}
-		Set<String> S = new LinkedHashSet<>(AL);
-		ArrayList<String> AL1 = new ArrayList<>(S);
-		System.out.println(AL1.get(AL1.size() - 1));
-	}
+    public static void main(String[] args) {
+        FindDestinationCity findDestinationCity = new FindDestinationCity();
+        String firstInput[][] = {{"London", "Newyork"}, {"Newyork", "Lima"}, {"Lima", "Sao Paulo"}};
+        String secondInput[][] = {{"B", "C"}, {"D", "B"}, {"C", "A"}};
+        String thirdInput[][] = {{"A", "Z"}};
+        System.out.println("\n-------1st array of city------------");
+        findDestinationCity.destinationCity(firstInput);
+        System.out.println("\n-------2nd array of city------------");
+        findDestinationCity.destinationCity(secondInput);
+        System.out.println("\n-------3rd array of city------------");
+        findDestinationCity.destinationCity(thirdInput);
+    }
 }
