9d5c60b68f6b72c636e786f77e06f701a75ed9d6
deepakghadge <deepak.ghadge@excellarate.com>
2023-04-06 12:19:38 +0530

    Updated code as per the PR comments

--- src/mediumComplexityProblem7/SortArray.java
+++ src/mediumComplexityProblem7/SortArray.java
@@ -1,52 +1,24 @@
 package mediumComplexityProblem7;
 
+import java.util.Arrays;
+
 public class SortArray {
     public void ascendingSort(int[] inputArray) {
         int firstCounter;
-        int secondCounter;
-        int temp = 0;
-
-        //Displaying elements of original array
-        System.out.println("Elements of original array: ");
-        for (firstCounter = 0; firstCounter < inputArray.length; firstCounter++) {
-            System.out.print(inputArray[firstCounter] + " ");
-        }
-
-        //Sort elements of original array in ascending order
-        for (firstCounter = 0; firstCounter < inputArray.length; firstCounter++) {
-            for (secondCounter = firstCounter + 1; secondCounter < inputArray.length; secondCounter++) {
-                if (inputArray[firstCounter] > inputArray[secondCounter]) {
-                    temp = inputArray[firstCounter];
-                    inputArray[firstCounter] = inputArray[secondCounter];
-                    inputArray[secondCounter] = temp;
-                }
+        int secondCounter = 0;
+        Arrays.sort(inputArray);
+        for (firstCounter = 0; firstCounter < inputArray.length - 1; firstCounter++) {
+            if (inputArray[firstCounter] != inputArray[firstCounter + 1]) {
+                inputArray[secondCounter] = inputArray[firstCounter];
+                secondCounter++;
             }
         }
-        System.out.println();
+        inputArray[secondCounter] = inputArray[inputArray.length - 1];
 
-        //Displaying elements of original array after ascending sort
-        System.out.println("Elements of original array sorted in ascending order: ");
-        for (firstCounter = 0; firstCounter < inputArray.length; firstCounter++) {
-            System.out.print(inputArray[firstCounter] + " ");
-        }
-
-        //Remove duplicates element from Ascending sort Array
-        int[] tempArray = new int[inputArray.length];
-        int checkDuplicateCounter;
-        int tempArrayCounter = 0;
-        for (checkDuplicateCounter = 0; checkDuplicateCounter < inputArray.length - 1; checkDuplicateCounter++) {
-            if (inputArray[checkDuplicateCounter] != inputArray[checkDuplicateCounter + 1]) {
-                tempArray[tempArrayCounter] = inputArray[checkDuplicateCounter];
-                tempArrayCounter++;
-            }
-        }
-        tempArray[tempArrayCounter] = inputArray[inputArray.length - 1];
-        System.out.println();
-
-        //Displaying elements from unique ascending sort array
+        //Displaying unique ascending sort elements from input array
         System.out.println("Unique ascending sort array elements:");
-        for (firstCounter = 0; firstCounter <= tempArrayCounter; firstCounter++) {
-            System.out.print(tempArray[firstCounter] + " ");
+        for (firstCounter = 0; firstCounter <= secondCounter; firstCounter++) {
+            System.out.print(inputArray[firstCounter] + " ");
         }
     }
 
