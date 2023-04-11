9914a6516a922369ce5e10d3012fcd2a1a8798d2
deepakghadge <deepak.ghadge@excellarate.com>
2023-04-11 12:45:21 +0530

    Added code to sort and remove duplicate elements from single loop

--- src/mediumComplexityProblem7/SortArray.java
+++ src/mediumComplexityProblem7/SortArray.java
@@ -2,9 +2,10 @@ package mediumComplexityProblem7;
 
 public class SortArray {
     public void ascendingSort(int[] inputArray) {
-        // Sort elements of original array in ascending order
-        int firstCounter;
-        for (firstCounter = 0; firstCounter < inputArray.length; firstCounter++) {
+        int uniqueArrayCounter = 0;
+        int[] uniqueArray = new int[inputArray.length];
+        // Sort the array in ascending order with unique element
+        for (int firstCounter = 0; firstCounter < inputArray.length; firstCounter++) {
             for (int secondCounter = firstCounter + 1; secondCounter < inputArray.length; secondCounter++) {
                 if (inputArray[firstCounter] > inputArray[secondCounter]) {
                     int temp = inputArray[firstCounter];
@@ -12,22 +13,18 @@ public class SortArray {
                     inputArray[secondCounter] = temp;
                 }
             }
-        }
-        // Remove duplicates element from Ascending sort Array
-        int checkDuplicateCounter;
-        int tempArrayCounter = 0;
-        for (checkDuplicateCounter = 0; checkDuplicateCounter < inputArray.length - 1; checkDuplicateCounter++) {
-            if (inputArray[checkDuplicateCounter] != inputArray[checkDuplicateCounter + 1]) {
-                inputArray[tempArrayCounter] = inputArray[checkDuplicateCounter];
-                tempArrayCounter++;
+            if (uniqueArrayCounter == 0) {
+                uniqueArray[uniqueArrayCounter] = inputArray[firstCounter];
+                uniqueArrayCounter++;
+            } else if (uniqueArray[uniqueArrayCounter - 1] != inputArray[firstCounter]) {
+                uniqueArray[uniqueArrayCounter] = inputArray[firstCounter];
+                uniqueArrayCounter++;
             }
         }
-        inputArray[tempArrayCounter] = inputArray[inputArray.length - 1];
-
         //Displaying unique ascending sort elements from input array
         System.out.println("Unique ascending sort array elements:");
-        for (checkDuplicateCounter = 0; checkDuplicateCounter <= tempArrayCounter; checkDuplicateCounter++) {
-            System.out.print(inputArray[checkDuplicateCounter] + " ");
+        for (int uniqueArrayIndex = 0; uniqueArrayIndex < uniqueArrayCounter; uniqueArrayIndex++) {
+            System.out.print(uniqueArray[uniqueArrayIndex] + " ");
         }
     }
 
